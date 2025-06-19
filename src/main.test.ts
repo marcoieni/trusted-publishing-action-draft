8851ffd15841f3ab942b2c71853eba7a933d4daa
Marco Ieni <11428655+marcoieni@users.noreply.github.com>
2025-06-19 10:56:34 +0200

    test missing env variable error (#15)

---
+++ src/main.test.ts
@@ -0,0 +1,9 @@
+import { expect, test } from "vitest";
+import * as main from "./main.js";
+
+test("returns the correct endpoint URL", async () => {
+    // The environment variable `ACTIONS_ID_TOKEN_REQUEST_URL` is not set.
+    await expect(main.run()).rejects.toThrowError(
+        "Please ensure the 'id-token' permission is set to 'write' in your workflow. For more information, see: https://docs.github.com/en/actions/security-for-github-actions/security-hardening-your-deployments/about-security-hardening-with-openid-connect#adding-permissions-settings",
+    );
+});
